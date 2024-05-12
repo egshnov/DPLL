9923b3281f79c55c6851c4c32586c0a96eb4d2f4
shalasheg <shalasheg@gmail.com>
2024-05-12 18:21:40 +0300

    Add execution time evaluation

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <chrono>
 
 #include "CNF.h"
 #include "DPLL.h"
@@ -20,7 +21,12 @@ int main(int argc, char *argv[]) {
         return -1;
     }
 
+    std::chrono::steady_clock::time_point t1 = std::chrono::steady_clock::steady_clock::now();
     bool solved = solver::DPLL(cnf);
+    std::chrono::steady_clock::time_point t2 = std::chrono::steady_clock::now();
+    std::chrono::duration<double> time_span = duration_cast<std::chrono::duration<double>>(t2 - t1);
+
+
     if (!solved) {
         std::cout << "s UNSATISFIABLE" << std::endl;
     } else {
@@ -37,5 +43,6 @@ int main(int argc, char *argv[]) {
         }
         std::cout << "0" << std::endl;
     }
+    //std::cout << "elapsed time: " << time_span.count() << std::endl;
     return 0;
 }
